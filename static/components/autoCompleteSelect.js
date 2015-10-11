82eafb7e1970b0a3fb69f0cc0d8d782ee109f4e8
1heart <yixin1996@gmail.com>
2015-10-11 04:44:51 -0400

    ugly but working select

--- static/components/autoCompleteSelect.js
+++ static/components/autoCompleteSelect.js
@@ -10,7 +10,7 @@ var AutoCompleteSelect = React.createClass({displayName: 'AutoCompleteSelect',
 	},
 
 	handleClick: function(e) {
-		console.log(e);
+		this.setState({currUser: e.target.innerText});
 	},
 
 	render: function() {
@@ -24,23 +24,23 @@ var AutoCompleteSelect = React.createClass({displayName: 'AutoCompleteSelect',
             	return l.toLowerCase().match( searchString );
             });
             for (var i=0; i<libraries.length; i++) {
-            	libraries[i] = <li onClick={this.handleClick(i)}>{libraries[i]}</li>
+            	libraries[i] = <li onClick={this.handleClick}>{libraries[i]}</li>
             }
 
 	        return 	<div>
-				        <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Type here" />
+				        <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Search here for recipient ID" />
 				        <ul> 
 				        	{libraries}
 
 				        </ul>
-			        <PayForm />
+			        <PayForm currUser={this.state.currUser}/>
 
 			        </div>
 			        ;
         }
 
         return 	<div>
-			        <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Type here" />
+			        <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Search here for recipient ID" />
 			    <PayForm />
 		        </div>
 		        ;
@@ -49,11 +49,13 @@ var AutoCompleteSelect = React.createClass({displayName: 'AutoCompleteSelect',
 });
 
 var PayForm = React.createClass({ displayName: 'PayForm',
-	getInitialState: function() {
-		return {};
-	},
 	render: function() {
-		return <div>FORM</div>
+		return (
+			<form className="payForm">
+				Pay <input  style={{'display': 'inline'}} type="number" /> BTC to { this.props.currUser || 'a friend'} <input  style={{'display': 'inline'}} type="hidden" value={this.props.currUser} /> for 
+				<input style={{'display': 'inline'}} type="text" />
+			</form>
+			);
 	}
 
 });
