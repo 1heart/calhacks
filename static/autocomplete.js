dd74bdfedd0644af5bfe30b9a8e6ad04532667a1
1heart <yixin1996@gmail.com>
2015-10-11 03:04:56 -0400

    autocomplete part 1

---
+++ static/autocomplete.js
@@ -0,0 +1,12 @@
+var autocomplete = function(listOfNames, currString) {
+	var resultArray = [];
+	for (i = 0; i < listOfNames.length; i++) {
+		if (listOfNames[i].indexOf(currString) > -1) {
+			resultArray.push(listOfNames[i]);
+		}
+	}
+	return resultArray;
+}
+
+
+
